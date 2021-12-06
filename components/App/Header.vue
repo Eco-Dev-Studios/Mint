7a54abc8db5ca766c967d2eacca2829fd25d45a0
cesswhite <acessloop@gmail.com>
2021-12-06 17:24:50 -0600

    Move componens to folder more descriptive

---
+++ components/App/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header
+    id="header"
+    class="h-auto bg-true-gray-900 bg-opacity-10 w-full py-4 fixed backdrop-filter backdrop-blur-md"
+  >
+    <app-layout size="xl" class="grid gap-10 grid-cols-2 items-center">
+      <div>
+        <app-logo />
+      </div>
+      <nav class="grid gap-4 grid-cols-4">
+        <a
+          href=""
+          class="font-semibold text-center text-lg text-gray-800 no-underline"
+          >Home</a
+        >
+        <a
+          href=""
+          class="font-semibold text-center text-lg text-gray-800 no-underline"
+          >About</a
+        >
+        <a
+          href=""
+          class="font-semibold text-center text-lg text-gray-800 no-underline"
+          >Works</a
+        >
+        <a
+          href=""
+          class="font-semibold text-center text-lg text-gray-800 no-underline"
+          >Contact</a
+        >
+      </nav>
+    </app-layout>
+  </header>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,10 @@
 <template>
   <div class="h-auto w-full">
     <!--Header-->
-    <header
-      id="header"
-      class="h-auto bg-true-gray-900 bg-opacity-10 w-full py-4 fixed backdrop-filter backdrop-blur-md"
-    >
-      <Layout size="xl" class="grid gap-10 grid-cols-2 items-center">
-        <div>
-          <Logo />
-        </div>
-        <nav class="grid gap-4 grid-cols-4">
-          <a
-            href=""
-            class="font-semibold text-center text-lg text-gray-800 no-underline"
-            >Home</a
-          >
-          <a
-            href=""
-            class="font-semibold text-center text-lg text-gray-800 no-underline"
-            >About</a
-          >
-          <a
-            href=""
-            class="font-semibold text-center text-lg text-gray-800 no-underline"
-            >Works</a
-          >
-          <a
-            href=""
-            class="font-semibold text-center text-lg text-gray-800 no-underline"
-            >Contact</a
-          >
-        </nav>
-      </Layout>
-    </header>
+    <app-header></app-header>
     <!--Index-->
-    <Layout size="xl" class="pt-18">
+
+    <app-layout size="xl" class="pt-18">
       <main class="h-screen w-full relative">
         <div class="animate-animated animate-fade-in w-1/2 center-y">
           <h1 class="font-bold font-sans mb-4 text-7xl text-gray-900">
@@ -53,15 +23,12 @@
           >
         </div>
       </main>
-    </Layout>
+    </app-layout>
   </div>
 </template>
 
 <script>
 export default {
-  data: () => ({
-    scrollY: 0,
-  }),
   mounted() {
     window.addEventListener("scroll", this.handleScroll);
   },
